078560722dc2b7af79f99a8ab0fb3a21f20dbaf6
someSOAP <andrey.meg@gmail.com>
2024-07-16 16:33:41 +0500

    products draft

---
+++ bso-spa/src/components/HomeHeader.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import { Header, Nav, Navbar } from "rsuite";
+import { Link } from "react-router-dom";
+import { CART_ROUTE, PRODUCTS_ROUTE } from "@/constants";
+import { Exit } from "@rsuite/icons";
+import { useAppDispatch } from "@/hooks";
+import { resetAuthStateAction } from "@/store/authSlice";
+
+export const HomeHeader: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const handleLogOut = () => {
+    dispatch(resetAuthStateAction());
+  };
+
+  return (
+    <Header>
+      <Navbar appearance="inverse">
+        <Navbar.Brand>
+          <span className="text-2xl">🤗</span>
+        </Navbar.Brand>
+        <Nav>
+          <Nav.Item as={Link} to={PRODUCTS_ROUTE}>
+            Products
+          </Nav.Item>
+          <Nav.Item as={Link} to={CART_ROUTE}>
+            Cart
+          </Nav.Item>
+        </Nav>
+        <Nav pullRight>
+          <Nav.Item onClick={handleLogOut} icon={<Exit />} />
+        </Nav>
+      </Navbar>
+    </Header>
+  );
+};
+
+export default HomeHeader;
--- bso-spa/src/routes/AuthenticatedRoot.tsx
+++ bso-spa/src/routes/AuthenticatedRoot.tsx
@@ -1,39 +1,14 @@
 import { FC } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import { Container, Header, Content, Navbar, Nav } from "rsuite";
-import { Exit } from "@rsuite/icons";
-import { useAppDispatch } from "@/hooks";
-import { resetAuthStateAction } from "@/store/authSlice";
-import { CART_ROUTE, PRODUCTS_ROUTE } from "@/constants";
+import { Container, Content } from "rsuite";
+
+import { HomeHeader } from "@/components";
 
 export const AuthenticatedRoot: FC = () => {
-  const dispatch = useAppDispatch();
-
-  const handleLogOut = () => {
-    dispatch(resetAuthStateAction());
-  };
-
   return (
     <Container className="h-full overflow-hidden flex flex-col">
-      <Header>
-        <Navbar appearance="inverse">
-          <Navbar.Brand>
-            <span className="text-2xl">🤗</span>
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Item as={Link} to={PRODUCTS_ROUTE}>
-              Products
-            </Nav.Item>
-            <Nav.Item as={Link} to={CART_ROUTE}>
-              Cart
-            </Nav.Item>
-          </Nav>
-          <Nav pullRight>
-            <Nav.Item onClick={handleLogOut} icon={<Exit />} />
-          </Nav>
-        </Navbar>
-      </Header>
+      <HomeHeader />
       <Content className="flex 1 overflow-hidden">
         <Outlet />
       </Content>
